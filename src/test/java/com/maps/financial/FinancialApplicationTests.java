b244e57565963f70ba55b45725822228b837d82c
Elisson Silva <elissonfs@gmail.com>
2020-07-08 08:28:16 -0300

    first commit

---
+++ src/test/java/com/maps/financial/FinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maps.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
